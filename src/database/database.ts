9c5d6893cd90917c6a235ce637ae5f2a1e6d1905
Shahnaaz Rahamatullah <rahamsha@gmail.com>
2023-11-30 22:58:06 +0000

    chore: add config to externalise application config/settings

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,4 +1,17 @@
-import { Sequelize } from "sequelize";
+import { Dialect, Sequelize } from "sequelize";
 
-// TODO: This should be external config
-export const sequelize = new Sequelize("sqlite::memory:");
+export let sequelize = new Sequelize("sqlite::memory:");
+if (process.env.NODE_ENV !== "test") {
+	sequelize = new Sequelize(
+		process.env.DB_NAME ?? "bookshop",
+
+		process.env.DB_USERNAME ?? "",
+
+		process.env.DB_PASSWORD ?? "",
+		{
+			host: process.env.DB_HOST ?? "",
+			port: parseInt(process.env.DB_PORT as string) ?? 5432,
+			dialect: (process.env.DB_DIALECT as Dialect) ?? "postgres",
+		}
+	);
+}
